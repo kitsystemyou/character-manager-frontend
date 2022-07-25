4c824620a0aae15f959720cfa71e5c9ce9d0aa9b
kazuhisa.senju <kazuhisa.senju@gmail.com>
2022-07-26 01:06:56 +0900

    feat/4: 能力値の表以外の入力欄を作成した。

--- character-manager/src/components/creations/CharacterStatus.jsx
+++ character-manager/src/components/creations/CharacterStatus.jsx
@@ -1,33 +1,140 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import { styled } from '@mui/material/styles'
-import Typography from "@material-ui/core/Typography";
-import ButtonBase from "@mui/material/ButtonBase";
+import FormControl from "@mui/material/FormControl";
+import TextField from '@mui/material/TextField'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from "@mui/material/Paper";
 
-const Icon = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
+function createData(name, str, con, pow, dex, app, siz, int, edu, hp, mp) {
+  return { name, str, con, pow, dex, app, siz, int, edu, hp, mp};
+}
+
+const rows = [
+  createData('初期値', '','','','','','','','','',''),
+  createData('増減値', '','','','','','','','','',''),
+  createData('一時的', '','','','','','','','','',''),
+  createData('現在値', '','','','','','','','','',''),
+];
 
 export default function CharacterStatus(){
   return(
     <Grid container spacing={2}>
-      <Grid item  >
-        <ButtonBase sx={{width:128, height: 128}}>
-          <Icon alt="complex" src="/coharu.png" />
-        </ButtonBase>
-      </Grid>
       <Grid item sm container>
         <Grid item xs container direction="column">
           <Grid item xs>
-            <Typography variant="h3">
-              山田　花子
-            </Typography>
-            <Typography variant="h5" gutterBottom>
-              システム：CoC
-            </Typography>
+            <TableContainer sx={{backgroundColor:'white'}}>
+              <Table showColumnRightBorder aria-label="status table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell> </TableCell>
+                    <TableCell align="center">STR</TableCell>
+                    <TableCell align="center">CON</TableCell>
+                    <TableCell align="center">POW</TableCell>
+                    <TableCell align="center">DEX</TableCell>
+                    <TableCell align="center">APP</TableCell>
+                    <TableCell align="center">SIZ</TableCell>
+                    <TableCell align="center">INT</TableCell>
+                    <TableCell align="center">EDU</TableCell>
+                    <TableCell align="center">HP </TableCell>
+                    <TableCell align="center">MP </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell align="center">初期値</TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField align="right" id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                    <TableCell align="right">            
+                      <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+                        <TextField id="height" variant="outlined"/>
+                      </FormControl>  
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={2} container>
+            <Grid item xs={4} sx={{pr:1}}>
+              <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+              <TextField id="idea" label="アイデア" variant="outlined"/>
+              </FormControl>  
+            </Grid> 
+            <Grid item xs={4} sx={{pl:1}}>         
+              <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+              <TextField id="knowledge" label="知識" variant="outlined"/>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4} sx={{pl:1}}>         
+              <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+              <TextField id="damageBonus" label="ダメージ・ボーナス" variant="outlined"/>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid item xs={2} container>
+            <Grid item xs={4} sx={{pr:1}}>
+              <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+              <TextField id="initSan" label="初期正気度" variant="outlined"/>
+              </FormControl>  
+            </Grid> 
+            <Grid item xs={4} sx={{pl:1}}>         
+              <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+              <TextField id="currentSan" label="正気度" variant="outlined"/>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4} sx={{pl:1}}>         
+              <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
+              <TextField id="luck" label="幸運" variant="outlined"/>
+              </FormControl>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
