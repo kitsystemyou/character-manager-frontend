a26963887e41ffed26e21b8ba64091489066a97f
kazuhisa.senju <kazuhisa.senju@gmail.com>
2023-02-26 17:28:50 +0900

    WIP: キャラクター一覧画面をリスト形式ではなくカードで表示する形式にする

--- character-manager/src/components/pages/CharacterList.jsx
+++ character-manager/src/components/pages/CharacterList.jsx
@@ -5,6 +5,7 @@ import { styled } from '@mui/material/styles';
 import CharacterContainer from "../modules/CharacterContainer";
 import Header from "../modules/Header";
 import Search from "../modules/Search";
+import CharacterCard from "../modules/CharacterCard";
 
 const Item = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#F9F4EF',
@@ -15,44 +16,39 @@ const Item = styled(Box)(({ theme }) => ({
   textalign: 'left',  
 }));
 
+
+
 const CharacterList = () => {
+    const characterID = [
+        {id: 1},
+        {id: 2},
+        {id: 3},
+        {id: 4}
+    ]
     return(
         <div>
+            <Header />
+            {/* 検索欄はmui v5に対応できてないので一旦保留 */}
+            {/* <Search/> */}
+            <hr />
             <div>
-                <Header />
+                <CharacterCard />
             </div>
-            <div className='search'>
-                {/* 検索欄 */}
-                <Search/>
-            </div>
-            <hr></hr>
             <div className='stack'>
                 {/* キャラクター一覧 */}
-                <Stack
-                direction="column"
-                justifyContent="center"
-                alignItems="stretch">
-                    {/* データベースから持ってきて、Item1の中にいい感じに入れる */}
-                    <Item sx={{ boxShadow: 3, border:2, borderColor:'#8c7851', borderRadius: 5}}>
-                        <CharacterContainer />
-                    </Item>                     
-                    <Item sx={{ boxShadow: 3, border:2, borderColor:'#8c7851', borderRadius: 5}}>
-                        <CharacterContainer />
-                    </Item> 
-                    <Item sx={{ boxShadow: 3, border:2, borderColor:'#8c7851', borderRadius: 5}}>
-                        <CharacterContainer />
-                    </Item>                     
-                    <Item sx={{ boxShadow: 3, border:2, borderColor:'#8c7851', borderRadius: 5}}>
-                        <CharacterContainer />
-                    </Item> 
-                    <Item sx={{ boxShadow: 3, border:2, borderColor:'#8c7851', borderRadius: 5}}>
-                        <CharacterContainer />
-                    </Item>                     
-                    <Item sx={{ boxShadow: 3, border:2, borderColor:'#8c7851', borderRadius: 5}}>
-                        <CharacterContainer />
-                    </Item> 
+                <Stack direction="column" justifyContent="center" alignItems="stretch">
+                    {/* データベースから持ってきて、Item1の中にいい感じに入れる */
+                        characterID.map(cId => {
+                            return(
+                                <Item sx={{ boxShadow: 3, border:2, borderColor:'#8c7851', borderRadius: 5}} key = {cId.id}>
+                                    <CharacterContainer />
+                                </Item>
+                            )      
+                        })
+                    }
                 </Stack>
             </div>
+           
         </div>
     );
 }
