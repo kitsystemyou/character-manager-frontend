5f4e4263b8c9230e9926b226522fc8a6b7763dfa
kazuhisa.senju <kazuhisa.senju@gmail.com>
2023-06-15 23:14:36 +0900

    draft: 技能値のコンポーネントを修正

--- character-manager/src/components/creations/CharacterSkills.jsx
+++ character-manager/src/components/creations/CharacterSkills.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import BasicSkillsEditableTable from "../modules/EditableTable/BasicSkillsEditableTable";
 import BattleSkillsEditableTable from "../modules/EditableTable/BattleSkillsEditableTable";
 import { Typography } from "@mui/material";
 
-export default function CharacterSkills() {
+export default function CharacterSkills(props) {
+    useEffect(() => props.setCharacterSkills(props.characterSkills), [props]);
+
     return (
         <Grid item xs container direction="column">
             <Typography variant="h4" style={{ textalign: 'left' }} sx={{ mt: 2, mb: 2 }}>基本技能</Typography>
             <Grid item container sx={{ mb: 2 }}>
-                <BasicSkillsEditableTable />
+                <BasicSkillsEditableTable 
+                    characterSkills = {props.characterSkills}
+                    setCharacterSkills = {props.setCharacterSkills}/>
             </Grid>
             <Typography variant="h4" style={{ textalign: 'left' }} sx={{ mt: 2, mb: 2 }}>戦闘技能</Typography>
             <Grid item container sx={{ mb: 2 }}>
-                <BattleSkillsEditableTable />
+                <BattleSkillsEditableTable 
+                    characterSkills = {props.characterSkills}
+                    setCharacterSkills = {props.setCharacterSkills}/>
             </Grid>
         </Grid>
     );
--- character-manager/src/components/modules/EditableTable/BasicSkillsEditableTable.jsx
+++ character-manager/src/components/modules/EditableTable/BasicSkillsEditableTable.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import * as fields from "../ConstantTableFields";
 import _ from 'lodash';
 
-export default function BasicSkillsEditableTable() {
+export default function BasicSkillsEditableTable(props) {
+    useEffect(() => props.setCharacterSkills(props.characterSkills), [props]);
     const [status, setStatus] = useState(rows);
 
     const changeCell = (v) => {
@@ -14,8 +15,23 @@ export default function BasicSkillsEditableTable() {
         newValue[idx][v.field] = v.value;
         newValue[idx].summary = sum;
         setStatus(newValue);
+        setTableValue(sum, v.field)
     }
 
+    const setTableValue = (sum, field) => {
+        field=="STR" ? props.setCharacterStatus({...props.characterStatus, str:sum})
+        : field =="CON" ? props.setCharacterStatus({...props.characterStatus, con:sum})
+        : field =="POW" ? props.setCharacterStatus({...props.characterStatus, pow:sum, init_san:sum*5,luck:sum*5})
+        : field =="DEX" ? props.setCharacterStatus({...props.characterStatus, dex:sum})
+        : field =="APP" ? props.setCharacterStatus({...props.characterStatus, app:sum})
+        : field =="SIZ" ? props.setCharacterStatus({...props.characterStatus, size:sum})
+        : field =="INT" ? props.setCharacterStatus({...props.characterStatus, int:sum, idea:sum*5})
+        : field =="EDU" ? props.setCharacterStatus({...props.characterStatus, edu:sum, knowledge:sum*5})
+        : field =="HP" ? props.setCharacterStatus({...props.characterStatus, hp:sum})
+        : field =="MP" ? props.setCharacterStatus({...props.characterStatus, mp:sum})
+        : field =="db" ? props.setCharacterStatus({...props.characterStatus, damage_bonus:sum})
+        : console.log(sum,field)
+    }
     return (
         <div style={{ width: '100%' }}>
             <DataGrid
