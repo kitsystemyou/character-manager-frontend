d98b06deedf033c02797b80da7294c6d7d6e11b7
kazuhisa.senju <kazuhisa.senju@gmail.com>
2023-11-25 00:24:49 +0900

    feat/30 追加情報の追加とINTの修正

--- character-manager/src/components/creations/AdditionalInfoReadOnly.jsx
+++ character-manager/src/components/creations/AdditionalInfoReadOnly.jsx
@@ -4,55 +4,52 @@ import FormControl from "@mui/material/FormControl";
 import TextField from '@mui/material/TextField'
 
 
-export default function AdditionalInfo(){
+export default function AdditionalInfo(props){
   return(
     <Grid container spacing={2}>
       <Grid item sm container>
           <Grid item xs={12}>
             <FormControl fullWidth sx={{ m: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="eduBackground" label="学校・学位" defaultValue="帝京平成大学" variant="outlined" InputProps={{readOnly: true}}/>
+              <TextField id="eduBackground" label="学校・学位" value = {props.additionalInfo.edu_background} variant="outlined" InputProps={{readOnly: true}}/>
             </FormControl>
           </Grid>
           <Grid item xs={12}>
             <FormControl fullWidth sx={{ m: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="mentalDisorder" label="精神的な障害" defaultValue="なし" variant="outlined" InputProps={{readOnly: true}}/>
+              <TextField id="mentalDisorder" label="精神的な障害" value={props.additionalInfo.mental_disorder} variant="outlined" InputProps={{readOnly: true}}/>
             </FormControl>
           </Grid>
           <Grid item xs={12} container>
             <Grid item xs={6} sx={{pr:1}}>
               <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="height" label="身長" defaultValue="193" variant="outlined" InputProps={{readOnly: true}}/>
+              <TextField id="height" label="身長" value={props.additionalInfo.height} variant="outlined" InputProps={{readOnly: true}}/>
               </FormControl>  
             </Grid> 
             <Grid item xs={6} sx={{pl:1}}>         
               <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="weight" label="体重" defaultValue="ひみつ" variant="outlined" InputProps={{readOnly: true}}/>
+              <TextField id="weight" label="体重" value={props.additionalInfo.weight} variant="outlined" InputProps={{readOnly: true}}/>
               </FormControl>
             </Grid>
           </Grid>
           <Grid item xs={12} container>
             <Grid item xs={4} sx={{pr:1}}>
               <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="hairColor" label="髪の色" defaultValue="黒" variant="outlined" InputProps={{readOnly: true}}/>
+              <TextField id="hairColor" label="髪の色" value={props.additionalInfo.hair_color} variant="outlined" InputProps={{readOnly: true}}/>
               </FormControl>  
             </Grid> 
             <Grid item xs={4} sx={{pl:1}}>         
               <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="eyeColor" label="瞳の色" defaultValue="黒" variant="outlined" InputProps={{readOnly: true}}/>
+              <TextField id="eyeColor" label="瞳の色" value={props.additionalInfo.eye_color} variant="outlined" InputProps={{readOnly: true}}/>
               </FormControl>
             </Grid>
             <Grid item xs={4} sx={{pl:1}}>         
               <FormControl sx={{ m: 1 , width: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="skinColor" label="肌の色" defaultValue="肌色" variant="outlined" InputProps={{readOnly: true}}/>
+              <TextField id="skinColor" label="肌の色" value={props.additionalInfo.skin_color} variant="outlined" InputProps={{readOnly: true}}/>
               </FormControl>
             </Grid>
           </Grid>
           <Grid item xs>
             <FormControl fullWidth sx={{ m: 1 , backgroundColor:'white' }} variant="outlined">
-              <TextField id="memo" label="その他・メモ欄" variant="outlined" defaultValue="エンジニアに必要なのは筋肉。筋肉があればプログラムが書ける。
-健康のために始めたキックボクシングにはまり週7でジムに通う。
-まじめな性格が災いし仕事のストレスでメンタルをやっている。
-筋肉も鬱には勝てない。" rows={5} multiline InputProps={{readOnly: true}}/>
+              <TextField id="memo" label="その他・メモ欄" variant="outlined" value={props.additionalInfo.memo} rows={5} multiline InputProps={{readOnly: true}}/>
             </FormControl>
           </Grid>
         </Grid>
